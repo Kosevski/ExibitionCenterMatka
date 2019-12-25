3c670daece7ff35e0a71119cef51663f9de5cf3f
Kosevski <kosevski.a@gmiaol.com>
2019-12-25 14:42:20 +0100

    update

--- Matka/app/src/main/java/com/example/matka/ExploreMatkaUI/ExploreExpoFragment.java
+++ Matka/app/src/main/java/com/example/matka/ExploreMatkaUI/ExploreExpoFragment.java
@@ -1,31 +1,32 @@
 package com.example.matka.ExploreMatkaUI;
 
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.matka.R;
-import com.google.android.material.tabs.TabLayout;
+import com.example.matka.scan.MainScannerActivity;
 import com.viewpagerindicator.CirclePageIndicator;
-import com.viewpagerindicator.TitlePageIndicator;
-import com.viewpagerindicator.UnderlinePageIndicator;
-
-import static com.viewpagerindicator.TitlePageIndicator.*;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ExploreExpoFragment extends Fragment {
+
+    public final int CUSTOMIZED_REQUEST_CODE = 0x0000ffff;
+    ImageView play, pause, stop, replay;
+    String result;
 
     public ExploreExpoFragment() {
         // Required empty public constructor
@@ -89,7 +90,97 @@ public class ExploreExpoFragment extends Fragment {
         viewPager.setAdapter(pagerAdapter);
         mIndicator.setViewPager(viewPager);
 
+                Button scannerbtn = rootView.findViewById(R.id.scannerbtn);
+        scannerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MainScannerActivity.class);
+                startActivity(intent);
+         }
+        });
+
+
+
+        final MediaPlayer mp = MediaPlayer.create(getContext(), R.raw.danceoftheknights);
+
+        play = rootView.findViewById(R.id.play_audio);
+        pause = rootView.findViewById(R.id.pause_audio);
+        stop = rootView.findViewById(R.id.stop_audio);
+        replay = rootView.findViewById(R.id.replay_audio);
+
+
+        Bundle bundle = this.getArguments();
+        if(bundle!=null){
+           result = bundle.getString("ResultContentsKey");
+        }
+
+        if (result != null && result.equals("Test")) {
+            mp.start();
+        }
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.pause();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.stop();
+            }
+        });
+
+        replay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.reset();
+            }
+        });
+
         return rootView;
     }
 
+
+//    public void scanMarginScanner(View view) {
+//        IntentIntegrator integrator = new IntentIntegrator(getActivity());
+//        integrator.setOrientationLocked(false);
+//        integrator.setCaptureActivity(BarcodeCaptureActivity.class);
+//        integrator.initiateScan();
+//    }
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        if (requestCode != CUSTOMIZED_REQUEST_CODE && requestCode != IntentIntegrator.REQUEST_CODE) {
+//            // This is important, otherwise the result will not be passed to the fragment
+//            super.onActivityResult(requestCode, resultCode, data);
+//            return;
+//        }
+//        switch (requestCode) {
+//            case CUSTOMIZED_REQUEST_CODE: {
+//                Toast.makeText(getContext(), "REQUEST_CODE = " + requestCode, Toast.LENGTH_LONG).show();
+//                break;
+//            }
+//            default:
+//                break;
+//        }
+//
+//        IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
+//
+//        if(result.getContents() == null) {
+//            Log.d("MainActivity", "Cancelled scan");
+//            Toast.makeText(getContext(), "Cancelled", Toast.LENGTH_LONG).show();
+//        } else {
+//            Log.d("MainActivity", "Scanned");
+//            Toast.makeText(getContext(), "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+//        }    }
 }
+
--- Matka/app/src/main/java/com/example/matka/ExploreMatkaUI/ExploreMatkaActivity.java
+++ Matka/app/src/main/java/com/example/matka/ExploreMatkaUI/ExploreMatkaActivity.java
@@ -1,13 +1,15 @@
 package com.example.matka.ExploreMatkaUI;
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-
-import android.os.Bundle;
 
 import com.example.matka.R;
 
 public class ExploreMatkaActivity extends AppCompatActivity {
+//    ImageView play, pause, stop, replay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,48 @@ public class ExploreMatkaActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         ExploreMatkaCenterFragment mainExploreFragment = new ExploreMatkaCenterFragment(fragmentManager);
         fragmentManager.beginTransaction().replace(R.id.frame_layout, mainExploreFragment).commit();
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.danceoftheknights);
+
+//        play = findViewById(R.id.play_audio);
+//        pause = findViewById(R.id.pause_audio);
+//        stop = findViewById(R.id.stop_audio);
+//        replay =findViewById(R.id.replay_audio);
+//
+//
+//        play.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mp.start();
+//            }
+//        });
+//
+//        pause.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mp.pause();
+//            }
+//        });
+//
+//        stop.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mp.stop();
+//            }
+//        });
+//
+//        replay.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mp.reset();
+//            }
+//        });
+
+        if((getIntent().getStringExtra("ResultContents"))!=null){
+        if((getIntent().getStringExtra("ResultContents")).equals("Test")){
+            mp.start();
+            ExploreExpoFragment fragment = new ExploreExpoFragment();
+            fragmentManager.beginTransaction().replace(R.id.frame_layout, fragment).commit();
+        }}
     }
 }
